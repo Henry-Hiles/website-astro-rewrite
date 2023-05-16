4f896a60f544b89800dc84eef5fa673fd4431b9b
Henry-Hiles <henry@henryhiles.com>
2023-05-16 12:08:12 -0400

    fix issue with redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({ trailingSlash: "never" })
+export default defineConfig({
+	site: "https://astro.henryhiles.com",
+	build: {
+		format: "file"
+	}
+})
