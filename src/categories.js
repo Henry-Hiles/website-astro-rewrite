83f39c615cf4a0a0f9313ccf673f576b8f2112ed
Henry-Hiles <henry@henryhiles.com>
2023-06-23 14:57:07 -0400

    Done, just need to fill out descriptions

--- src/categories.js
+++ src/categories.js
@@ -1,7 +1,23 @@
 export default Object.entries({
-	all: { title: "All", default: true },
-	linux: { title: "Linux" },
-	web: { title: "Web Development" },
-	bot: { title: "Discord Bot Development" },
-	other: { title: "Other" }
+	all: {
+		title: "All",
+		default: true,
+		description:
+			"Hello, and welcome to my blog. I post about Discord Bot Development, Web Development, Linux, and other Tech-related subjects. You can filter and search in the sidebar."
+	},
+	linux: {
+		title: "Linux",
+		description:
+			"Linux is an operating system that has existed since the 1990s, and is used extensively, from Android in your phones which is linux-based to the over 40% of websites running on linux."
+	},
+	web: { title: "Web Development", description: "Web Development" },
+	bot: {
+		title: "Discord Bot Development",
+		description: "Discord Bot Development"
+	},
+	other: {
+		title: "Other",
+		description:
+			"This section of my blog is for all my articles that don't fit into any other catagories."
+	}
 })
