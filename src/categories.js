4bf53a4b14995491ef40819d762bf58ff00d771e
Henry-Hiles <henry@henryhiles.com>
2023-06-23 14:13:48 -0400

    improve code

---
+++ src/categories.js
@@ -0,0 +1,7 @@
+export default Object.entries({
+	all: { title: "All", default: true },
+	linux: { title: "Linux" },
+	web: { title: "Web Development" },
+	bot: { title: "Discord Bot Development" },
+	other: { title: "Other" }
+})
