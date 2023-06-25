f1d0f9ea9d31b8e5bd96917d5784b3b380b8a833
Henry-Hiles <henry@henryhiles.com>
2023-06-25 09:56:37 -0400

    Done, just need to add first article

--- src/categories.js
+++ src/categories.js
@@ -2,18 +2,24 @@ export default Object.entries({
 	all: {
 		title: "All",
 		default: true,
+		descriptionTitle: "Welcome to the blog!",
 		description:
-			"Hello, and welcome to my blog. I post about Discord Bot Development, Web Development, Linux, and other Tech-related subjects. You can filter and search in the sidebar."
+			"Hello, and welcome to my blog. I post about Discord Bot Development, Web Development, Linux, and other tech-related subjects. You can filter and search in the sidebar, and click on an article to read more."
 	},
 	linux: {
 		title: "Linux",
 		description:
-			"Linux is an operating system that has existed since the 1990s, and is used extensively, from Android in your phones (which is linux-based), to the over 40% of websites running on linux."
+			"Linux is an operating system that has existed since the 1990s, and is what I use on my PC daily. Linux is used extensively, from Android (which is Linux-based) in your phones, to the over 40% of websites running on Linux."
 	},
-	web: { title: "Web Development", description: "Web Development" },
+	web: {
+		title: "Web Development",
+		description:
+			"Web Development impacts all of our lives daily, from ordering a package from Amazon to looking at this blog. There are over 2 billion (2,000,000,000) websites in the world, and that number is only increasing. [You can order a custom website from me here](/#hire)."
+	},
 	bot: {
 		title: "Discord Bot Development",
-		description: "Discord Bot Development"
+		description:
+			"Discord is a chat app which has over 300 million registered accounts. There are Discord Bots which can automate tasks or add new features to improve your experience. I post articles about Discord Bots here. I can also [make a discord bot for you](/discord-bot-developer)."
 	},
 	other: {
 		title: "Other",
